d1d8624789d6fa36160c036fadb636885b882ee7
iigonzoii <a.gonzalez418@gmail.com>
2021-05-21 11:57:43 -0600

    router links working, grid working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
@@ -7,78 +7,83 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { useEffect, useState } from "react";
 
-
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-// const [portfolioArray, setPortfolioArray] = useState([])
-let portfolioArray = [{
-title:"Employee Directory",
-gitHub:"https://github.com/iigonzoii/EmployeeDirectory",
-deployedSite:"https://iigonzoii.github.io/EmployeeDirectory/",
-img:"",
-description:"React App geared towards managment that allows the user to view nonsensitive information about their employees. You can filter names by typing or sort employees alphabetically "
-},
-{
-  title:"Weather Dashboard",
-  gitHub:"https://github.com/iigonzoii/WeatherDashBoard",
-  deployedSite:"https://iigonzoii.github.io/WeatherDashBoard/",
-  img:"",
-  description:"Use this application to find the current forecast and the upcoming fiveday forecast as well"
-},
-{
-  title:"WorkDay Scheduler",
-  gitHub:"https://github.com/iigonzoii/WorkDayScheduler",
-  deployedSite:"https://iigonzoii.github.io/WorkDayScheduler/",
-  img:"",
-  description:"Responsive scheduler that breaks down your work day hour blocks by color. Itemizing your meetings by past, present, and future"
-}]
+  // const [portfolioArray, setPortfolioArray] = useState([])
+  let portfolioArray = [
+    {
+      title: "Employee Directory",
+      gitHub: "https://github.com/iigonzoii/EmployeeDirectory",
+      deployedSite: "https://iigonzoii.github.io/EmployeeDirectory/",
+      img: "https://firebasestorage.googleapis.com/v0/b/project-images-5c20b.appspot.com/o/reactDirScaled2.jpg?alt=media&token=e704d7ed-9068-481e-b409-e2fe44b3874f",
+      description:
+        "React App geared towards managment that allows the user to view nonsensitive information about their employees. You can filter names by typing or sort employees alphabetically ",
+    },
+    {
+      title: "Weather Dashboard",
+      gitHub: "https://github.com/iigonzoii/WeatherDashBoard",
+      deployedSite: "https://iigonzoii.github.io/WeatherDashBoard/",
+      img: "",
+      description:
+        "Use this application to find the current forecast and the upcoming fiveday forecast as well",
+    },
+    {
+      title: "WorkDay Scheduler",
+      gitHub: "https://github.com/iigonzoii/WorkDayScheduler",
+      deployedSite: "https://iigonzoii.github.io/WorkDayScheduler/",
+      img: "placeholder",
+      description:
+        "Responsive scheduler that breaks down your work day hour blocks by color. Itemizing your meetings by past, present, and future",
+    },
+  ];
+
+  
+
+  // ? Project three structure, css not working(stucture), duplicate folders
 
   return (
     <>
-    <Header />
-    <Router>
-    {/* <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/mystack">MyStack</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        </div> */}
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
+      <div className="grid grid-cols-2 gap-4 portfolio-grid">
+        {portfolioArray.map((each, index) => {
+          return (
+            <div key={index}>
+              <h1>{each.title}</h1>
+              <a href={each.gitHub} rel="noreferrer" target="_blank">
+                Github
+              </a>
+              <p>{each.deployedSite}</p>
+              <img src={each.img} alt={each.title} />
+              <p>{each.description}</p>
+            </div>
+          );
+        })}
+      </div>
 
-        <Route path="/portfolio">
-          <Portfolio />
-        </Route>
 
-        <Route path="/mystack">
-          <MyStack />
-        </Route>
+      <Header />
 
-        <Route path="/contact">
-          <Contact />
-        </Route>
+      <Router>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
 
-      </Switch>
-    </Router>
-<Footer />
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+
+          <Route path="/mystack">
+            <MyStack />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function Header () {
     return (
       <>
+      <Router>
       <ul class="flex bg-black ">
   <li class="mr-6">
-    <a class="text-blue-500 hover:text-blue-800" href="About">About</a>
+    <Link to="/about">
+    <span class="text-blue-500 hover:text-blue-800">About</span>
+    </Link>
   </li>
   <li class="mr-6">
     <a class="text-blue-500 hover:text-blue-800" href="Portfolio">Portfolio</a>
@@ -16,6 +19,7 @@ function Header () {
     <a class="text-blue-500 hover:text-blue-800" href="Mystack">My Stack</a>
   </li>
 </ul>
+</Router>
 
       </>
     );
