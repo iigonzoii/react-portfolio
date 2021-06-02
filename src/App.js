6175d20b93ebf80e37fec9720865b510e49d3718
iigonzoii <a.gonzalez418@gmail.com>
2021-06-02 16:49:09 -0600

    got hashrouting set up

--- src/App.js
+++ src/App.js
@@ -10,63 +10,29 @@ import { useEffect, useState } from "react";
 
 import React from "react";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { HashRouter, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
   // const [portfolioArray, setPortfolioArray] = useState([])
-  let portfolioArray = [
-    {
-      title: "Employee Directory",
-      gitHub: "https://github.com/iigonzoii/EmployeeDirectory",
-      deployedSite: "https://iigonzoii.github.io/EmployeeDirectory/",
-      img: "https://firebasestorage.googleapis.com/v0/b/project-images-5c20b.appspot.com/o/reactDirScaled2.jpg?alt=media&token=e704d7ed-9068-481e-b409-e2fe44b3874f",
-      description:
-        "React App geared towards managment that allows the user to view nonsensitive information about their employees. You can filter names by typing or sort employees alphabetically ",
-    },
-    {
-      title: "Weather Dashboard",
-      gitHub: "https://github.com/iigonzoii/WeatherDashBoard",
-      deployedSite: "https://iigonzoii.github.io/WeatherDashBoard/",
-      img: "https://firebasestorage.googleapis.com/v0/b/project-images-5c20b.appspot.com/o/wdb3fb.jpg?alt=media&token=aeadc92a-972f-4203-9068-a24ef70602c2",
-      description:
-        "Use this application to find the current forecast and the upcoming fiveday forecast as well",
-    },
-    {
-      title: "WorkDay Scheduler",
-      gitHub: "https://github.com/iigonzoii/WorkDayScheduler",
-      deployedSite: "https://iigonzoii.github.io/WorkDayScheduler/",
-      img: "https://firebasestorage.googleapis.com/v0/b/project-images-5c20b.appspot.com/o/better%20copy.jpg?alt=media&token=0d96e8e2-a5d1-4446-a325-52624fef95d1",
-      description:
-        "Responsive scheduler that breaks down your work day hour blocks by color. Itemizing your meetings by past, present, and future",
-    },
-    {
-      title: "Team Profile Generator",
-      gitHub: "https://github.com/iigonzoii/TeamProfileGenerator",
-      deployedSite:
-        "https://drive.google.com/file/d/1RtkfeQF_KHjy0BkcvBe0SF9fTBpma_kL/view",
-      img: "https://firebasestorage.googleapis.com/v0/b/project-images-5c20b.appspot.com/o/tpg2.jpg?alt=media&token=6caa525d-6722-4986-a309-1ce65924f375",
-      description:
-        "Command line application using Node.js to create team profiles for your business",
-    },
-  ];
+  
 
   return (
     <>
-      <Header />
-      <HashRouter basename="/">
+      <Router>
         <div>
+      <Header />
           <Route exact path="/" component={AboutPage} />
-          <Route path="/about" component={AboutPage} />
+          <Route exact path="/about" component={AboutPage} />
           <Route exact path="/mystack" component={MyStack} />
           <Route exact path="/contact" component={ContactPage} />
           <Route
             exact
             path="/portfolio"
             component={PortfolioPage}
-            portfolioArray={portfolioArray}
+            
           />
         </div>
-      </HashRouter>
+      </Router>
       {/* <Router>
         
         <Switch>
@@ -91,7 +57,7 @@ function App() {
           </Route>
         </Switch>
       </Router> */}
-      );
+     
       <Footer />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,43 @@
 // ! my link isnt working here, i have it working on my project though. 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import { HashRouter, Route, Link } from "react-router-dom";
+import {Link} from "react-router-dom"
 function Header () {
     return (
       <>
-      <Router>
+     
         <header className="sm:text-sm md:text-2xl lg:text-5xl  ">
       <ul className="flex bg-black lg:p-12 md:p-8 sm:p-4 ">
 
-  <li className="mr-6 md:ml-4">
+  <li className="mr-6 md:ml-4 text-blue-500 hover:text-blue-800">
   
-     {/* <Link to="about"> */}
-    <a className="text-blue-500 hover:text-blue-800 " href="about">About</a>
-    {/* </Link> */}
+     <Link to="/about" className={window.location.pathname === "/about"}>
+     About
+    </Link>
      
   </li>
 
-  <li className="mr-6 md:ml-4">
-  
-    <a className="text-blue-500 hover:text-blue-800" href="Portfolio">Portfolio</a>
- 
+  <li className="mr-6 md:ml-4 text-blue-500 hover:text-blue-800">
+  <Link to="/portfolio" className={window.location.pathname === "/portfolio"}
+    >Portfolio
+ </Link>
   </li>
 
-  <li className="mr-6 md:ml-4">
-    <a className="text-blue-500 hover:text-blue-800" href="Contact">Contact</a>
+  <li className="mr-6 md:ml-4 text-blue-500 hover:text-blue-800">
+  <Link to="/contact" className={window.location.pathname === "/contact"}>
+    Contact
+    </Link>
   </li>
 
-  <li className="mr-6 md:ml-4">
-    <a className="text-blue-500 hover:text-blue-800" href="Mystack">My Stack</a>
+  <li className="mr-6 md:ml-4 text-blue-500 hover:text-blue-800">
+  <Link to="/mystack" className={window.location.pathname === "/mystack "}>
+    My Stack
+    </Link>
   </li>
 
 </ul>
 </header>
-</Router>
+
 
       </>
     );
